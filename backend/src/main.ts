a823dcd7752299a156271224bff603bc0b308f94
Melisaates <101855839+Melisaates@users.noreply.github.com>
2025-12-06 18:00:17 +0300

    feat(main): update server port configuration and logging

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || '3000';
+  await app.listen(parseInt(port, 10) || 3000);
+  console.log(`Backend is running on ${port}`);
 }
 bootstrap();
